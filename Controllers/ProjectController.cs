2d8660ec2a290d0db3a56e8e1a54dfa366014f1e
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-01-05 14:44:08 +0100

    make it possible to delete projects

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -37,7 +38,7 @@ namespace ProjectManager.Controllers
 
         // GET Project/AddProject
         [HttpGet]
-        public async Task<IActionResult> AddProject() 
+        public async Task<IActionResult> AddProject()
         {
             return View();
         }
@@ -54,6 +55,34 @@ namespace ProjectManager.Controllers
                 });
                 _context.SaveChanges();
             }
+            return RedirectToAction(nameof(ListProjects));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteProject(string id)
+        {
+            Project project = _context.Projects.Where(p => p.Id == id).FirstOrDefault();
+            if (project == null)
+            {
+                return RedirectToAction(nameof(DeleteProject), new { id = id });
+            }
+            return View(new DeleteProjectViewModel
+            {
+                Project = project
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DestroyProject(string id, bool delete)
+        {
+            Project project = _context.Projects.Where(p => p.Id == id).FirstOrDefault();
+            if (project == null || !delete)
+            {
+                // Ignore
+                return RedirectToAction(nameof(ListProjects));
+            }
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(ListProjects));
         }
     }
---
+++ Models/ProjectViewModels/DeleteProjectViewModel..cs
@@ -0,0 +1,8 @@
+namespace ProjectManager.Models.ProjectViewModels
+{
+    public class DeleteProjectViewModel
+    {
+        public bool Delete { get; set; }
+        public Project Project { get; set; }
+    }
+}
