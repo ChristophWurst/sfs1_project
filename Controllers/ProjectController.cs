52340cb3fe6d16411f45d2d459f7978b826bd318
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-01-05 10:30:54 +0100

    list projects only for logged in users

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,4 +1,6 @@
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProjectManager.Models;
 using ProjectManager.Models.ProjectViewModels;
@@ -9,15 +11,24 @@ namespace ProjectManager.Controllers
     {
         private ProjectManagerContext _context;
 
-        public ProjectController(ProjectManagerContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProjectController(ProjectManagerContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET /Project/ListProjects
         [HttpGet]
-        public IActionResult ListProjects()
+        public async Task<IActionResult> ListProjects()
         {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            if (user == null)
+            {
+               return RedirectToAction(nameof(AccountController.Login), new { Controller = "Account" });
+            }
+
             return View(new ListProjectViewModel()
             {
                 Projects = _context.Projects.ToList()
