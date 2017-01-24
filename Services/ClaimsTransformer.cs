a5d0c214a9b09183c505fbd003f6050cba238687
Stefan Rösch <Stefan Rsch>
2017-01-24 21:05:21 +0100

    only gmail users get the yolo claim

--- Services/ClaimsTransformer.cs
+++ Services/ClaimsTransformer.cs
@@ -1,13 +1,22 @@
-using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using System.Linq;
 
 public class ClaimsTransformer : IClaimsTransformer
 {
     public Task<ClaimsPrincipal> TransformAsync(ClaimsTransformationContext context)
     {
-        ((ClaimsIdentity)context.Principal.Identity).AddClaim(new Claim("Yolo", "true"));
+        var prince = context.Principal;
+        if (prince.Claims.Any(c =>
+        {
+            return c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
+                     && c.Value.EndsWith("@gmail.com");
+        }))
+        {
+            ((ClaimsIdentity)context.Principal.Identity).AddClaim(new Claim("Yolo", "true"));
+        }
+
         return Task.FromResult(context.Principal);
     }
 
